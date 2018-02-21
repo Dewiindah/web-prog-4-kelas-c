959baaf6d7c815827a626e64e2f5d7c5e081aea7
rifaiabdulgani <rifai515@gmail.com>
2018-02-21 18:22:37 +0700

    tambah profil

---
+++ README.md
@@ -0,0 +1,5 @@
+#web-prog-1-kelas-c
+
+Nama : Rifai Abdul Gani
+Kelas : 4c
+Prodi : D4 Teknik Informatika
---
+++ profil.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nama = "Rifai Abdul Gani";
+    $kelas = "4C";
+    $prodi = "D4 Teknik Informatika";
+
+    echo "<h1>Latihan Web Programming<h1><br>";
+    echo $nama; echo "<br>";
+    echo $kelas; echo "<br>";
+    echo $prodi; echo "<br>";
+
+?>
+
